90ebb178f38fa27217ed127f17ad951a8a387c89
dietergeerts <dieter@dworks.be>
2020-08-13 11:56:21 +0200

    Make unit tests work on Windows too

--- test.js
+++ test.js
@@ -3,6 +3,7 @@ const remark = require('remark')
 const lint = require('remark-lint')
 const vfile = require('vfile')
 const appropriateHeading = require('./')
+const path = require('path')
 
 const processorWithOptions = (options) => remark().use(lint).use(appropriateHeading, options)
 const processor = processorWithOptions()
@@ -45,8 +46,8 @@ const tooManyNewLinesAfterHtmlComment = `<!-- some comment-->
 `
 
 test('remark-lint-appropriate-heading', (t) => {
-  var fp = '~/heading/readme.md'
-  var mwfp = '~/multi-word-heading/readme.md'
+  var fp = path.join('~', 'heading', 'readme.md')
+  var mwfp = path.join('~', 'multi-word-heading', 'readme.md')
 
   t.deepEqual(
     processor.processSync(vfile({path: fp, contents: ok})).messages.map(String),
@@ -62,31 +63,31 @@ test('remark-lint-appropriate-heading', (t) => {
 
   t.deepEqual(
     processor.processSync(vfile({path: fp, contents: invalidHeading})).messages.map(String),
-    ['~/heading/readme.md:1:1-1:10: Heading \'invalid\' is not the directory name'],
+    [`${fp}:1:1-1:10: Heading \'invalid\' is not the directory name`],
     'should warn for invalid headings (mismatch in directory and heading)'
   )
 
   t.deepEqual(
     processor.processSync(vfile({path: mwfp, contents: multiWordHeading})).messages.map(String),
-    ['~/multi-word-heading/readme.md:1:1-1:21: Heading \'multi-word heading\' is not the directory name'],
+    [`${mwfp}:1:1-1:21: Heading \'multi-word heading\' is not the directory name`],
     'should warn for invalid headings (mismatch in directory and heading)'
   )
 
   t.deepEqual(
     processor.processSync(vfile({path: fp, contents: missingHeading})).messages.map(String),
-    ['~/heading/readme.md:1:1-1:10: Document must start with a heading'],
+    [`${fp}:1:1-1:10: Document must start with a heading`],
     'should warn if the first thing is not a heading'
   )
 
   t.deepEqual(
     processor.processSync(vfile({path: fp, contents: tooLowHeading})).messages.map(String),
-    ['~/heading/readme.md:2:1-2:10: Heading does not start at beginning of document'],
+    [`${fp}:2:1-2:10: Heading does not start at beginning of document`],
     'should warn if the first heading is not on the first line'
   )
 
   t.deepEqual(
     processor.processSync(vfile({path: fp, contents: empty})).messages.map(String),
-    ['~/heading/readme.md:1:1-1:1: Document must start with a heading'],
+    [`${fp}:1:1-1:1: Document must start with a heading`],
     'should warn without heading'
   )
 
@@ -110,7 +111,7 @@ test('remark-lint-appropriate-heading', (t) => {
 
   t.deepEqual(
     processor.processSync(vfile({path: fp, contents: tooManyNewLinesAfterHtmlComment})).messages.map(String),
-    ['~/heading/readme.md:4:1-4:10: Heading does not start at beginning of document'],
+    [`${fp}:4:1-4:10: Heading does not start at beginning of document`],
     'should warn if too much newlines after html comment'
   )
 
